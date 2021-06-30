e9f5d1600f7275d164371abfc95cfd3d1d75030f
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-06-30 14:25:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,23 @@ recognition.onresult = function run(event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
+    speak();
 }
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    var speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+var camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 330,
+    height: 220,
+    image_format: "jpeg",
+    jpeg_quality: 90
+});
