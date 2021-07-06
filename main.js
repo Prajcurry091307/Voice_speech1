d229ddffaa2176953351167bc49d24a678597acd
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-07-06 14:46:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -14,19 +14,28 @@ recognition.onresult = function run(event) {
     console.log(event);
 
     var Content = event.results[0][0].transcript;
-    console.log(Content);
-
+    
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    console.log(Content);
+    
+    if(Content = "take my selfie"){
+        console.log("taking the selfie")
+        speak();
+    }
+    
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
-    var speak_data = document.getElementById("textbox").value;
+    var speak_data = "Taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save(); 
+    },5000);
 }
 
 var camera = document.getElementById("camera");
@@ -37,3 +46,18 @@ Webcam.set({
     image_format: "jpeg",
     jpeg_quality: 90
 });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save()
+{
+  var link = document.getElementById("link");
+  var image = document.getElementById("selfie_image").src;
+  link.href = image;
+  link.click();
+}
